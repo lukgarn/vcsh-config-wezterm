4b1b6df81459e98f3e54e45f558f022360166f54
Łukasz Garncarek <lukgar@gmail.com>
2024-06-09 12:55:19 +0200

    Set font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,10 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Material (base16)'
+
+config.font = wezterm.font('JetBrains Mono NF', {weight = 'Medium', stretch = 'Expanded'})
+config.font_size = 10.0
 
 -- and finally, return the configuration to wezterm
 return config
