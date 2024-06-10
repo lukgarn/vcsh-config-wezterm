6cc0c6ae2575e1dd467933e28ac955f4c19e8a1b
Łukasz Garncarek <lukgar@gmail.com>
2024-06-10 20:28:44 +0200

    set font size to 12.0

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Material (base16)'
 
 config.font = wezterm.font('JetBrains Mono NF', {weight = 'Medium'})
-config.font_size = 11.0
+config.font_size = 12.0
 
 config.front_end = 'OpenGL'
 config.freetype_load_target = 'Light'
