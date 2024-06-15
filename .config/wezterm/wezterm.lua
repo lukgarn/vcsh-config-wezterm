793fb8eacc80d98c270f6a9573293bc3678aa1c8
Łukasz Garncarek <lukgar@gmail.com>
2024-06-15 11:38:22 +0200

    update font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,22 +1,19 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
+config.color_scheme = "Material (base16)"
 
--- For example, changing the color scheme:
-config.color_scheme = 'Material (base16)'
-
-config.font = wezterm.font('JetBrains Mono NF', {weight = 'Medium'})
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
 config.font_size = 12.0
 
-config.front_end = 'OpenGL'
-config.freetype_load_target = 'Light'
-config.freetype_render_target = 'HorizontalLcd'
-config.freetype_load_flags = 'NO_HINTING'
+config.front_end = "OpenGL"
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
+config.freetype_load_flags = "NO_HINTING"
 
 -- and finally, return the configuration to wezterm
 return config
-
